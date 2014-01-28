30516c0319c1be3b7c3b0096cf17b808ccdad5d8
shunirr <m@s5r.jp>
2014-01-28 17:46:28 +0900

    ハッシュの生成を乱数でやる

--- app.rb
+++ app.rb
@@ -1,26 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'rack'
-require 'digest/md5'
-require 'sdbm'
 
 module Gyazo
   class Controller < Sinatra::Base
-
     configure do
-      set :dbm_path, 'db/id'
       set :image_dir, 'public/images'
-      set :image_url, 'http://gyazo.send.sh/images'
+      set :image_url, 'http://static.s5r.jp/images'
     end
 
     post '/' do
       id = request[:id]
       data = request[:imagedata][:tempfile].read
-      hash = Digest::MD5.hexdigest(data).to_s.to_i(16).to_s(36)
-      dbm = SDBM.open(options.dbm_path, 0644)
-      dbm[hash] = id
+      hash = (rand * 10 ** 8).to_i.to_s(16)
       File.open("#{options.image_dir}/#{hash}.png", 'w'){|f| f.write(data)}
-
       "#{options.image_url}/#{hash}.png"
     end
   end
